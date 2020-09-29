70d055866b7bc83a1bbde758817db7f5c6c6a9a5
EikoSAIJOU334 <esaijou@iqb.u-tokyo.ac.jp>
2020-09-29 21:14:43 +0900

    rinkou_1_2.pyを中戸さんのを活かして直したが、関数内の変数を外で使う方法がわからないので書き出しがエラーになる

--- rinkou_1_2.py
+++ rinkou_1_2.py
@@ -6,23 +6,54 @@ import sys
 import argparse
 # argparse: プログラム実行時にコマンドラインで引数を受け取る処理を簡単に実装できる標準ライブラリ
 
-# Pandas、及び必要なライブラリのインポート
+# Pandasのインポート
 import pandas as pd
 
 # address.txtの読み込み
-# 変数名はadとした
-ad = pd.read_csv("address.txt", sep='\t')
+## 変数名はadとした
+## ad = pd.read_csv("address.txt", sep='\t')
 
-# 先頭を表示して読み込みの確認
-print('データの先頭を表示')
-print(ad.head)
+def read_file(path):
+    lines = []
+#    with open(path, "r", encoding="utf-8") as f:
+    lines = pd.read_csv(path, sep='\t', encoding="utf-8")
+#        lines = f.readlines()
+#        lines = [ln.strip(os.linesep) for ln in lines]
 
-# ファイルの行数をカウント、表示
-print('データの行数を表示')
-print(len(ad))
+    return lines
 
-# ちなみに119045と表示される
+# change this function that uses pandas library
+def count_lines(file_path):
+    lines = read_file(file_path)
+    return len(lines)
 
-# タブをスペースに置換したファイルを出力する
-# address_space.txtというファイル名にした
-ad.to_csv('address_space.txt', sep=' ', header=False, index=False)
+# main関数を定義 (to increase readability)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="show partial lines of file")
+    parser.add_argument("file", help="file path", type=str)
+    parser.add_argument("number", help="number of lines", type=int)
+
+    args = parser.parse_args()
+
+    filename = args.file
+    nlines = args.number
+    print('file: %s' % filename)
+    print('number: %d' % nlines)
+
+    len = count_lines(filename)
+
+    print('length is {}'.format(len))
+
+## 先頭を表示して読み込みの確認
+# print('データの先頭を表示')
+# print(ad.head)
+
+## ファイルの行数をカウント、表示
+# print('データの行数を表示')
+# print(len(ad))
+
+## ちなみに119045と表示される
+
+## タブをスペースに置換したファイルを出力する
+## address_space.txtというファイル名にした
+# lines.to_csv('{}_space.txt'.format(filename), sep=' ', header=False, index=False)
